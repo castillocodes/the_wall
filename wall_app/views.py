1e75e447d133cba59d845abc5ab82e9df8027415
castillocodes <janeirosergio@gmail.com>
2021-06-20 11:40:06 -0500

    Added templates and static folders

--- wall_app/views.py
+++ wall_app/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render (request, "index.html")
+
+def success(request):
+    if 'user' not in request.sesssion:
+        return redirect('/')
+    context = {
+        'wall_messages': Wall_Message.objects.all()
+    }
+    return render(request, "success.html", context)
